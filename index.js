d121369a3445f6c57a61e7a2e9f6997534ea9d2f
flow1981 <contact.f.stagliano@gmail.com>
2019-11-18 10:59:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution here!
+const append = ["Milo", "Otis", "Garfield"];
+const prepend = ["Milo", "Otis", "Garfield"];
+const removeFirst = ["Milo", "Otis", "Garfield"];
+const removeLast = ["Milo", "Otis", "Garfield"];
+
+//Append
+append.push("Odie");
+
+//Prepend
+prepend.unshift("Odie");
+
+//Remove Last
+removeLast.pop();
+
+//Remove First
+removeFirst.shift();
+
+console.log(append)
